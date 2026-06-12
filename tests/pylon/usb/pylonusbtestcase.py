61e29c9e7cce15ce948e00ebdd24574b89e93da7
Gau, Andreas <Andreas.Gau@baslerweb.com>
2026-06-12 12:54:53 +0000

    Apply suggestions from code review

--- tests/pylon/usb/pylonusbtestcase.py
+++ tests/pylon/usb/pylonusbtestcase.py
@@ -1,17 +1,7 @@
 import unittest
 from pypylon import pylon
-from pypylon.pylon import BaslerUsbDeviceClass
 
-
-def get_class_and_filter():
-    device_class = BaslerUsbDeviceClass
-    di = pylon.DeviceInfo()
-    di.SetDeviceClass(device_class)
-    return device_class, [di]
 
 class PylonTestCase(unittest.TestCase):
-    device_class, device_filter = get_class_and_filter()
-
-    def create_first(self):
-        tlf = pylon.TlFactory.GetInstance()
-        return pylon.InstantCamera(tlf.CreateFirstDevice(self.device_filter[0]))
+    def get_camera_traits(self):
+        return {pylon.DeviceClassKey: pylon.BaslerUsbDeviceClass}
